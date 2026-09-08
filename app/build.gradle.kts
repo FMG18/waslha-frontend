5d5b1e58764b32cdd5a54859078b3bfba0f78f78
FMG18 <48799138+FMG18@users.noreply.github.com>
2026-09-08 19:48:51 +0300

    feat: add Mapbox Maps SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,5 +47,7 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+    implementation("com.mapbox.maps:android-ndk27:11.30.0")
+    implementation("com.mapbox.extension:maps-compose-ndk27:11.30.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
