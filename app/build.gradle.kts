f87aa35eef280cbf1aa60f73fe0db8583b3c00c1
FMG18 <48799138+FMG18@users.noreply.github.com>
2026-09-14 00:34:59 +0300

    chore android release 1.4.9 versionCode 149

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.waslha.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 148
-        versionName = "1.4.8"
+        versionCode = 149
+        versionName = "1.4.9"
     }
 
     compileOptions {
